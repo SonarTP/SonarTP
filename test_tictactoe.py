de2079cb27ea6a2eabb730ac65f7c5eab27e878f
brichard01 <baptisterichard.centrale@gmail.com>
2025-12-05 15:34:13 +0100

    push plateau

--- test_tictactoe.py
+++ test_tictactoe.py
@@ -2,3 +2,7 @@ import tictactoe
 
 def test_retone():
     assert tictactoe.retone() == 1
+
+def test_init():
+    plateau = tictactoe.Plateau()
+    assert plateau.plateau == [[' '] * 3] * 3
--- tictactoe.py
+++ tictactoe.py
@@ -1,2 +1,6 @@
 def retone():
     return 1
+
+class Plateau:
+    def __init__(self):
+        self.plateau = [[' '] * 3] * 3
