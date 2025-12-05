5dfc1ea95c02b1338396793bf6c63b4323f092a3
brichard01 <baptisterichard.centrale@gmail.com>
2025-12-05 17:48:29 +0100

    assrt false

--- test_tictactoe.py
+++ test_tictactoe.py
@@ -5,7 +5,7 @@ def test_retone():
 
 def test_init():
     jeu = tictactoe.Jeu()
-    assert jeu.plateau == [[' '] * 3] * 3
+    assert jeu.plateau == [['_'] * 3] * 3
 
 def test_play():
     jeu = tictactoe.Jeu()
@@ -13,7 +13,28 @@ def test_play():
         jeu.play(i+1, 1)
     assert jeu.last_player
     assert jeu.plateau == [
-        ['O', ' ', ' '],
-        ['X', ' ', ' '],
-        ['O', ' ', ' '],
+        ['O', 'X', 'O'],
+        ['_', '_', '_'],
+        ['_', '_', '_'],
     ]
+
+def test_win_vertical():
+    jeu = tictactoe.Jeu()
+    for i in range(5):
+        x, y = i%2 + 1, i//2 + 1
+        jeu.play(x, y)
+        if i<4:
+            assert not jeu.check_win(x, y)
+    assert jeu.check_win(x, y)
+    
+def test_win_horizontal():
+    jeu = tictactoe.Jeu()
+    for i in range(5):
+        x, y = i//2 + 1, i%2 + 1
+        jeu.play(x, y)
+        if i<4:
+            assert not jeu.check_win(x, y)
+    assert jeu.check_win(x, y)
+
+def test_false():
+    assert False
--- tictactoe.py
+++ tictactoe.py
@@ -4,18 +4,41 @@ def retone():
 class Jeu:
     def __init__(self):
         self.plateau = [
-        [' ', ' ', ' '],
-        [' ', ' ', ' '],
-        [' ', ' ', ' '],
+        ['_', '_', '_'],
+        ['_', '_', '_'],
+        ['_', '_', '_'],
     ]
         self.last_player = False
 
     def play(self, x, y):
         assert x in [1, 2, 3]
         assert y in [1, 2, 3]
-        assert self.plateau[x-1][y-1] == ' '
+        assert self.plateau[y-1][x-1] == '_'
         if self.last_player:
-            self.plateau[x-1][y-1] = 'X'
+            self.plateau[y-1][x-1] = 'X'
         else:
-            self.plateau[x-1][y-1] = 'O'
+            self.plateau[y-1][x-1] = 'O'
         self.last_player = not self.last_player
+
+    def check_win(self, x, y):
+        play = self.plateau[y-1][x-1]
+        for i in range(1, 3):
+            if self.plateau[(y-1+i)%3][x-1] != play:
+                break
+            return True
+        for j in range(1, 3):
+            if self.plateau[y-1][(x-1+j)%3] != play:
+                break
+            return True
+        return False
+
+    def __repr__(self):
+        repr = '\n-Jeu-\n'
+        repr += '\n'.join([' '.join(line) for line in self.plateau])
+        return repr + '\n'
+
+if __name__=='__main__':
+    jeu = Jeu()
+    for i in range(5):
+        jeu.play(i%2+1, i//2+1)
+        print(jeu)
