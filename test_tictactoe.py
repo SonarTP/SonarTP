7d95c588c073b8e1ca0d6827297fbeb95e3be0e9
brichard01 <baptisterichard.centrale@gmail.com>
2025-12-05 15:41:23 +0100

    play

--- test_tictactoe.py
+++ test_tictactoe.py
@@ -6,3 +6,9 @@ def test_retone():
 def test_init():
     plateau = tictactoe.Plateau()
     assert plateau.plateau == [[' '] * 3] * 3
+
+def test_play():
+    plateau = tictactoe.Plateau()
+    for i in range(7):
+        plateau.play()
+    assert plateau.last_player
--- tictactoe.py
+++ tictactoe.py
@@ -4,3 +4,7 @@ def retone():
 class Plateau:
     def __init__(self):
         self.plateau = [[' '] * 3] * 3
+        self.last_player = False
+
+    def play(self):
+        self.last_player = not self.last_player
